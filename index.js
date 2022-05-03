b25e98f81899a23aab9087f4298ae9d8d218ac88
anacortutau <anacortutau@gmail.com>
2022-05-03 12:45:23 +0200

    intro-node

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    let sortItem = this.items.sort((item1,item2) =>{
+      if(item1 > item2){
+        return 1
+      }else{
+        return -1
+      }
+    })
+    this.length = sortItem.length
+  }
+  get(pos) {
 
-  get(pos) {}
+  }
 
   max() {}
 
